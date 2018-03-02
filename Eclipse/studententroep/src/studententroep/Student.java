885fe28d8946f61f4b7b15200b0cfa496fe53608
Simonvn1 <simon@vannoppen.com>
2018-03-02 11:44:25 +0100

    Les 9

---
+++ Eclipse/studententroep/src/studententroep/Student.java
@@ -0,0 +1,5 @@
+package studententroep;
+
+public class Student {
+
+}
---
+++ Eclipse/studententroep/src/studententroep/mainApp.java
@@ -0,0 +1,10 @@
+package studententroep;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
